0897265b7514822b1fe71c6a4fea483a6528097e
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-08-16 10:30:42 -0400

    x

--- world.lua
+++ world.lua
@@ -221,7 +221,7 @@ updateLeaderboard = function(entry)
 	if #listBlocksHit > 0 then
 		table.sort(listBlocksHit, function(a, b)
 			print(a.nbHits.value, b.nbHits.value)
-			return a.nbHits.value - b.nbHits.value
+			return a.nbHits.value > b.nbHits.value
 		end)
 		leaderboardTextHits.Text = ""
 		for i = 1, 10 do
