6eafd54a841d9b94ce82c328d7efe8bad4a0e756
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-08-20 15:35:43 -0400

    x

--- world.lua
+++ world.lua
@@ -639,9 +639,8 @@ initPlayer = function()
 	Player.pickaxe = Shape(Items.caillef.pickaxe)
 	Player:EquipRightHand(Player.pickaxe)
 	Player.pickaxe.Palette[1].Color = LEVEL_COLOR[2]
-	-- Player.pickaxe.Palette[2].Color = LEVEL_COLOR[3]
-	-- Player.pickaxe.Palette[3].Color = LEVEL_COLOR[4]
-	print(LEVEL_COLOR[1])
+	Player.pickaxe.Palette[2].Color = LEVEL_COLOR[3]
+	Player.pickaxe.Palette[3].Color = LEVEL_COLOR[4]
 	require("crosshair"):show()
 	Player.Avatar:loadEquipment({ type = "hair" })
 	Player.Avatar:loadEquipment({ type = "jacket" })
