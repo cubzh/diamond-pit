2ff2e5dc3eadf7720219b532d2a02648591b286b
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-08-21 15:38:14 -0400

    x

--- world.lua
+++ world.lua
@@ -194,7 +194,10 @@ createNewPlayer = function(key, position)
 		end
 		local dir = player.targetPosition - player.model.Position
 		dir:Normalize()
-		player.model.Position = player.model.Position + dir * dt * 10
+		player.model.Position = player.model.Position + dir * dt * 30
+		player.Forward = dir
+		player.Rotation.X = 0
+		player.Rotation.Z = 0
 		if (player.model.Position - player.targetPosition).SquaredLength <= 3 then
 			player.targetPosition = nil
 		end
