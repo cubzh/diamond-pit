bbcd6425876dbe6f043113e59caff183e197bf8b
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-09-11 11:06:00 +0200

    x

--- world.lua
+++ world.lua
@@ -928,7 +928,7 @@ Client.Tick = function(dt)
                     local impactPos = Camera.Position + Camera.Forward * impact.Distance
                     emitter.Position = impactPos
                     emitter:spawn(15)
-                    sfx(string.format("metal_clanging_%d", math.random(1, 5)), { Spatialized = false, Volume = 0.6 })
+                    sfx(string.format("wood_impact_%d", math.random(1, 5)), { Spatialized = false, Volume = 0.6 })
 
                     local playerPos = Player.Position + Number3(1, 1, 1) * 1000000
                     tickSinceSync = 0
