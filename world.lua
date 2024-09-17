993b0436915c11ef645dc42993a3000e808a3672
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-09-17 16:18:50 +0200

    x

--- world.lua
+++ world.lua
@@ -140,7 +140,7 @@ worldInfo = {
     rpc_url = "https://api.cartridge.gg/x/diamond-pit/katana",
     torii_url = "https://api.cartridge.gg/x/diamond-pit/torii",
     world = "0x6d5313d73973bb9bbd5c76f5a4d8f5f5f3f244c95596a01391de6a63f77f4b2",
-    actions = "diamond_pit-actions",
+    actions = "0x01e3d5078d20dbcd0457d93a9a201e1ba9b58e09a68a4d48b2b3a9894942e1ac",
     playerAddress = "0x657e5f424dc6dee0c5a305361ea21e93781fea133d83efa410b771b7f92b",
     playerSigningKey = "0xcd93de85d43988b9492bfaaff930c129fc3edbc513bb0c2b81577291848007",
 }
