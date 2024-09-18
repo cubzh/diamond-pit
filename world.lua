5756b30adec0d1fd11554c2f34b4f3f6a97a72f0
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-09-18 10:19:16 +0200

    x

--- world.lua
+++ world.lua
@@ -1176,6 +1176,8 @@ Client.OnWorldObjectLoad = function(obj)
             shadow.Position = obj.Position
             shadow.Pivot = obj.Pivot
             shadow.Scale = obj.Scale
+            shadow.Rotation = obj.Rotation
+            shadow.IsHidden = false
             for i = 1, #shadow.Palette do
                 -- shadow.Palette[i].Color = Color.Black
             end
@@ -1326,7 +1328,7 @@ function handleBlocksImpact(impact)
         local impactPos = Camera.Position + Camera.Forward * impact.Distance
         emitter.Position = impactPos
         emitter:spawn(15)
-        sfx(string.format("wood_impact_%d", math.random(1, 5)), { Spatialized = false, Volume = 0.6 })
+        sfx(string.format("wood_impact_%d", math.random(3, 5)), { Spatialized = false, Volume = 0.6 })
 
         local playerPos = Player.Position + Number3(1, 1, 1) * 1000000
         tickSinceSync = 0
