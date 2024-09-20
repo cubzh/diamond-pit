9829ea51facb37b1a6a5c484f679c60d6cadf730
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-09-20 16:25:46 +0200

    x

--- world.lua
+++ world.lua
@@ -319,6 +319,7 @@ end
 blocksModule.setBlockHP = function(self, block, hp, maxHP, blockType)
     if not block then return end
     if not self.chips[block.Coords.Z] or not self.chips[block.Coords.Z][block.Coords.Y] or not self.chips[block.Coords.Z][block.Coords.Y][block.Coords.X] then
+        print(hp, maxHP, blockType)
         if hp < maxHP then
             self:addChips(block, BLOCK_COLORS[blockType])
         else
