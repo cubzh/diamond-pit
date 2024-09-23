dc3e4c2993aac9041e3901b934aef36c7f087469
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-09-23 14:12:54 +0200

    x

--- world.lua
+++ world.lua
@@ -1569,7 +1569,6 @@ function updateBlocksColumn(key, rawColumn)
         local b = blocksModule.blockShape:GetBlock(column.x, z, column.y)
         blocksModule:setBlockHP(b, blockHp, BLOCKS_MAX_HP[blockType], blockType)
         local blockColor = BLOCK_COLORS[blockType]
-        -- if b then b:Remove() end
         if b and (blockHp == 0 or blockType == 0 or blockColor == nil) then
             if texturedBlocks[z] and texturedBlocks[z][column.y] and texturedBlocks[z][column.y][column.x] then
                 texturedBlocks[z][column.y][column.x]:RemoveFromParent()
