0d5c2e6c20497ab4719d3a5a9b0d09e59437bbbc
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-09-23 16:17:02 +0200

    x

--- world.lua
+++ world.lua
@@ -1567,23 +1567,24 @@ function updateBlocksColumn(key, rawColumn)
         local blockHp = blockInfo & 127
         local z = -(column.z_layer * 10 + k)
         local b = blocksModule.blockShape:GetBlock(column.x, z, column.y)
-        blocksModule:setBlockHP(b, blockHp, BLOCKS_MAX_HP[blockType], blockType)
-        local blockColor = BLOCK_COLORS[blockType]
         if blockType < 8 and b then
             b:Remove()
             b = nil
             blockHp = 0
-        end
-        if b and (blockHp == 0 or blockType == 0 or blockColor == nil) then
-            b:Remove()
-            if texturedBlocks[z] and texturedBlocks[z][column.y] and texturedBlocks[z][column.y][column.x] then
-                texturedBlocks[z][column.y][column.x]:RemoveFromParent()
-                texturedBlocks[z][column.y][column.x] = nil
+        else
+            blocksModule:setBlockHP(b, blockHp, BLOCKS_MAX_HP[blockType], blockType)
+            local blockColor = BLOCK_COLORS[blockType]
+            if b and (blockHp == 0 or blockType == 0 or blockColor == nil) then
+                b:Remove()
+                if texturedBlocks[z] and texturedBlocks[z][column.y] and texturedBlocks[z][column.y][column.x] then
+                    texturedBlocks[z][column.y][column.x]:RemoveFromParent()
+                    texturedBlocks[z][column.y][column.x] = nil
+                end
+            elseif b and b.Color ~= blockColor then
+                b:Replace(blockColor)
+            elseif not b and blockHp > 0 then
+                blocksModule.blockShape:AddBlock(blockColor, column.x, z, column.y)
             end
-        elseif b and b.Color ~= blockColor then
-            b:Replace(blockColor)
-        elseif not b and blockHp > 0 then
-            blocksModule.blockShape:AddBlock(blockColor, column.x, z, column.y)
         end
 
         -- if starknet block
