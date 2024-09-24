a70dbcf226717ab1ade605f76820cddea17de846
caillef <corentin.cailleaud@caillef.com>
2024-09-24 10:45:48 +0200

    x

--- world.lua
+++ world.lua
@@ -660,7 +660,7 @@ initSellingArea = function()
     text.IsUnlit = true
     text.Color = Color.Black
     text.Anchor = { 0.5, 0 }
-    text.Position = Number3(150, 25, 450)
+    text.Position = Number3(160, 30, 460)
     LocalEvent:Listen(LocalEvent.Name.Tick, function()
         text.Forward = Player.Forward
     end)
